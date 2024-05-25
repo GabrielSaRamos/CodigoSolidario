bf172f6fead52f667b6168a0fdd181ee6ebbdea2
Gabriel Ramos <ga.sa.ramos@hotmail.com>
2024-05-25 17:19:15 -0300

    Login
    Foi criado sistema de login!

---
+++ js/login.js
@@ -0,0 +1,46 @@
+async function validarLogin() {
+    event.preventDefault(); // Impede o envio padrão do formulário
+  
+    const nomeLogin = document.getElementById('nome_login').value;
+    const emailLogin = document.getElementById('email_login').value;
+    const senhaLogin = document.getElementById('senha_login').value;
+  
+    try {
+        let users = await buscarUsuarios();
+  
+        let user = users.find(user => user.email === emailLogin);
+  
+        let nome = users.find(user => user.nome === nomeLogin);
+  
+        if (nome && user && user.senha === senhaLogin) {
+            alert("Login bem-sucedido!");
+            // Redirecionar ou executar outras ações após login bem-sucedido
+            window.location.href = "index.html"; // Substitua pelo link desejado
+        } else {
+            alert("Dados incorretos! Insira corretamente!");
+        }
+    } catch (error) {
+        console.error("Erro ao buscar usuários: ", error);
+        alert("Erro ao tentar fazer login. Tente novamente mais tarde.");
+    }
+  }
+  
+  async function buscarUsuarios() {
+    let urlServer = "https://6641230ca7500fcf1a9faa98.mockapi.io/CodigoSolidario";
+  
+    let response = await fetch(urlServer, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
+  
+    if (!response.ok) {
+        throw new Error(`Erro na requisição: ${response.status}`);
+    }
+  
+    let users = await response.json();
+    return users;
+  
+  }
+  
